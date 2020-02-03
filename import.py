6034e67b982d594bbc5a751a128578b70c9300a1
DamielCowen <dbcohen7260@gmail.com>
2020-02-03 09:42:12 -0800

    import first working files
    src folder and notebook

---
+++ import.py
@@ -0,0 +1,19 @@
+import os
+import padas as pd
+
+
+
+
+class new_data():
+
+    def __init(self, path)__:
+        self.path = path
+        
+        
+    def open(self):
+        docs = []
+            with open(self.path)
+            with open('./reviews.json', 'r') as data_file:    
+    for line in data_file:
+        
+   
